753e451911c22ec9a9d8d559d318ec953e90787a
Rick Cooper <rick.cooper@upside.energy>
2021-06-30 18:25:57 +0100

    feat: 5.2

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -6,12 +6,16 @@ import {Switch} from '../switch'
 
 const callAll = (...fns) => (...args) => fns.forEach(fn => fn?.(...args))
 
+const toggleActionsTypes = {
+  TOGGLE: 'toggle',
+  RESET: 'reset',
+};
 function toggleReducer(state, {type, initialState}) {
   switch (type) {
-    case 'toggle': {
+    case toggleActionsTypes.TOGGLE: {
       return {on: !state.on}
     }
-    case 'reset': {
+    case toggleActionsTypes.RESET: {
       return initialState
     }
     default: {
@@ -25,8 +29,8 @@ function useToggle({initialOn = false, reducer = toggleReducer} = {}) {
   const [state, dispatch] = React.useReducer(reducer, initialState)
   const {on} = state
 
-  const toggle = () => dispatch({type: 'toggle'})
-  const reset = () => dispatch({type: 'reset', initialState})
+  const toggle = () => dispatch({type: toggleActionsTypes.TOGGLE})
+  const reset = () => dispatch({type: toggleActionsTypes.RESET, initialState})
 
   function getTogglerProps({onClick, ...props} = {}) {
     return {
@@ -57,7 +61,7 @@ function App() {
   const clickedTooMuch = timesClicked >= 4
 
   function toggleStateReducer(state, action) {
-    if (action.type === 'toggle' && clickedTooMuch) {
+    if (action.type === toggleActionsTypes.TOGGLE && clickedTooMuch) {
       return {on: state.on}
     }
     return toggleReducer(state, action);
